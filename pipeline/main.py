42e8d933e8ef26d09e940ed4ae5f87404576548c
Philip Igbeka <82071328+Chuks-Chuks@users.noreply.github.com>
2025-09-24 15:02:53 +0100

    Script to orchestrate ingestion to dumping in DB logic.

---
+++ pipeline/main.py
@@ -0,0 +1,24 @@
+from pipeline.mosaic_tiles import MosaicTiles
+from pipeline.etl_pipeline import ClipStates 
+from pipeline.analyze_loss import AnalyzeLoss
+from utils.log import get_logger
+
+log = get_logger()
+
+def main():
+    # Step 1: Mosaic Tiles
+    mosaic = MosaicTiles()
+    raster_files = mosaic.find_tiles()
+    log.info(f'Found {len(raster_files)} raster files for processing.')
+    # mosaic.create_mosaic(raster_files)
+
+    # Step 2: Clip Rasters to States
+    clipper = ClipStates()
+    clipper.clip_rasters_to_states()
+
+    # Step 3: Analyze Forest Loss by State
+    analyzer = AnalyzeLoss()
+    analyzer.analyze_loss_by_state()
+
+if __name__ == "__main__":
+    main()
